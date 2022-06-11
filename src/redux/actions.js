e0d024f214ce666d8d0e3bcb0411be4a5175faf8
manikanta1341 <medicharlamanikanta13@gmail.com>
2022-06-11 12:18:13 +0530

    first commit

--- src/redux/actions.js
+++ src/redux/actions.js
@@ -4,43 +4,39 @@ import { ActionTypes } from "../constants/constants"
 import jwt_decode from 'jwt-decode'
 
 
-export const Token_set=(response)=>async dispatch=>{
+export const Token_set = (response) => async dispatch => {
     try {
-<<<<<<< HEAD
-        console.log(response)
-=======
-//         console.log(response)
->>>>>>> cc1c146c524cdfe82b81765bd90fdd65674ae9e3
-        window.localStorage.setItem('token',response)
+        //         console.log(response)
+        window.localStorage.setItem('token', response)
         dispatch({
-            type:ActionTypes.SET_TOKEN,
-            payload:jwt_decode(response)
+            type: ActionTypes.SET_TOKEN,
+            payload: jwt_decode(response)
         })
     } catch (error) {
-        console.log("err:",error)
+        console.log("err:", error)
     }
 }
 
-export const ApiCall_msgs = (_id)=> async dispatch=>{
+export const ApiCall_msgs = (_id) => async dispatch => {
     try {
         const response = await axios.get(`${url}/inbox/${_id}`)
         // //console.log(response)
-        if(!response.data.msg){
-            dispatch({ type:ActionTypes.FETCH_MAILS, payload:response.data })
+        if (!response.data.msg) {
+            dispatch({ type: ActionTypes.FETCH_MAILS, payload: response.data })
             window.location.reload()
 
         }
     } catch (error) {
         throw error
     }
-} 
+}
 
-export const DeleteMsgsInbox =(uid,array)=>async dispatch=>{
+export const DeleteMsgsInbox = (uid, array) => async dispatch => {
     try {
-        const response = await axios.post(`${url}/inbox/bin/${uid}`,{
-            arr:array
+        const response = await axios.post(`${url}/inbox/bin/${uid}`, {
+            arr: array
         })
-        dispatch({type:ActionTypes.FETCH_MAILS,payload:response.data})
+        dispatch({ type: ActionTypes.FETCH_MAILS, payload: response.data })
         window.location.reload()
         //console.log(response)
     } catch (error) {
@@ -48,13 +44,13 @@ export const DeleteMsgsInbox =(uid,array)=>async dispatch=>{
     }
 }
 
-export const DeleteMsgsSent =(uid,array)=>async dispatch=>{
+export const DeleteMsgsSent = (uid, array) => async dispatch => {
     try {
-        const response = await axios.post(`${url}/sent/bin/${uid}`,{
-            arr:array
+        const response = await axios.post(`${url}/sent/bin/${uid}`, {
+            arr: array
         })
         //console.log(response)
-        dispatch({type:ActionTypes.FETCH_MAILS,payload:response.data})
+        dispatch({ type: ActionTypes.FETCH_MAILS, payload: response.data })
         window.location.reload()
 
     } catch (error) {
@@ -62,13 +58,13 @@ export const DeleteMsgsSent =(uid,array)=>async dispatch=>{
     }
 }
 
-export const DeleteMsgsDraft =(uid,array)=>async dispatch=>{
+export const DeleteMsgsDraft = (uid, array) => async dispatch => {
     try {
-        const response = await axios.post(`${url}/draft/bin/${uid}`,{
-            arr:array
+        const response = await axios.post(`${url}/draft/bin/${uid}`, {
+            arr: array
         })
         //console.log(response)
-        dispatch({type:ActionTypes.FETCH_MAILS,payload:response.data})
+        dispatch({ type: ActionTypes.FETCH_MAILS, payload: response.data })
         window.location.reload()
 
     } catch (error) {
@@ -76,127 +72,127 @@ export const DeleteMsgsDraft =(uid,array)=>async dispatch=>{
     }
 }
 
-export const DeleteMsgsStarred =(uid,array)=>async dispatch=>{
+export const DeleteMsgsStarred = (uid, array) => async dispatch => {
     try {
-        const response = await axios.post(`${url}/starred/bin/${uid}`,{
-            arr:array
+        const response = await axios.post(`${url}/starred/bin/${uid}`, {
+            arr: array
         })
-        dispatch({type:ActionTypes.FETCH_MAILS,payload:response.data})
+        dispatch({ type: ActionTypes.FETCH_MAILS, payload: response.data })
         window.location.reload()
     } catch (error) {
         throw error
     }
 }
 
-export const PermanentDelete =(uid,array)=>async dispatch=>{
+export const PermanentDelete = (uid, array) => async dispatch => {
     try {
         console.log("in Pdelete api call")
-        const response = await axios.post(`${url}/delete/${uid}`,{
-          arr:array
+        const response = await axios.post(`${url}/delete/${uid}`, {
+            arr: array
         })
         console.log(response.data)
-        dispatch({type:ActionTypes.FETCH_MAILS,payload:response.data})
+        dispatch({ type: ActionTypes.FETCH_MAILS, payload: response.data })
         window.location.reload()
     } catch (error) {
-        console.log("err:",error)
+        console.log("err:", error)
     }
 }
 
-export const StarredMsg = (uid,_id)=>async dispatch=>{
+export const StarredMsg = (uid, _id) => async dispatch => {
     try {
-        let response = await axios.patch(`${url}/starred/${uid}`,{
-            id:_id
+        let response = await axios.patch(`${url}/starred/${uid}`, {
+            id: _id
         })
-        dispatch({type:ActionTypes.FETCH_MAILS,payload:response.data})
-        window.location.reload()
-
-        
-    } catch (err) {
-        console.log("err:",err)
-    }
-}
-export const UnStarredMsg = (uid,_id)=>async dispatch=>{
-    try {
-        let response = await axios.patch(`${url}/unstarred/${uid}`,{
-            id:_id
-        })
-        dispatch({type:ActionTypes.FETCH_MAILS,payload:response.data})
+        dispatch({ type: ActionTypes.FETCH_MAILS, payload: response.data })
         window.location.reload()
 
 
     } catch (err) {
-        console.log("err:",err)
+        console.log("err:", err)
     }
 }
-export const StarredMsg_Sent = (uid,_id)=>async dispatch=>{
+export const UnStarredMsg = (uid, _id) => async dispatch => {
     try {
-        let response = await axios.patch(`${url}/sent/starred/${uid}`,{
-            id:_id
+        let response = await axios.patch(`${url}/unstarred/${uid}`, {
+            id: _id
         })
-        dispatch({type:ActionTypes.FETCH_MAILS,payload:response.data})
-        window.location.reload()
-
-        
-    } catch (err) {
-        console.log("err:",err)
-    }
-}
-export const UnStarredMsg_Sent = (uid,_id)=>async dispatch=>{
-    try {
-        let response = await axios.patch(`${url}/sent/unstarred/${uid}`,{
-            id:_id
-        })
-        dispatch({type:ActionTypes.FETCH_MAILS,payload:response.data})
+        dispatch({ type: ActionTypes.FETCH_MAILS, payload: response.data })
         window.location.reload()
 
 
     } catch (err) {
-        console.log("err:",err)
+        console.log("err:", err)
+    }
+}
+export const StarredMsg_Sent = (uid, _id) => async dispatch => {
+    try {
+        let response = await axios.patch(`${url}/sent/starred/${uid}`, {
+            id: _id
+        })
+        dispatch({ type: ActionTypes.FETCH_MAILS, payload: response.data })
+        window.location.reload()
+
+
+    } catch (err) {
+        console.log("err:", err)
+    }
+}
+export const UnStarredMsg_Sent = (uid, _id) => async dispatch => {
+    try {
+        let response = await axios.patch(`${url}/sent/unstarred/${uid}`, {
+            id: _id
+        })
+        dispatch({ type: ActionTypes.FETCH_MAILS, payload: response.data })
+        window.location.reload()
+
+
+    } catch (err) {
+        console.log("err:", err)
     }
 }
 
-export const MarkallRead = (string,_id)=>async dispatch=>{
+export const MarkallRead = (string, _id) => async dispatch => {
     try {
         let response = await axios.get(`${url}/allread/${_id}/?s=${string}`)
-        dispatch({type:ActionTypes.FETCH_MAILS,payload:response.data})
+        dispatch({ type: ActionTypes.FETCH_MAILS, payload: response.data })
         window.location.reload()
     } catch (err) {
-        console.log('err',err)
+        console.log('err', err)
     }
 }
-export const MarkallunRead = (string,_id)=>async dispatch=>{
+export const MarkallunRead = (string, _id) => async dispatch => {
     try {
         let response = await axios.get(`${url}/allunread/${_id}/?s=${string}`)
-        dispatch({type:ActionTypes.FETCH_MAILS,payload:response.data})
+        dispatch({ type: ActionTypes.FETCH_MAILS, payload: response.data })
         window.location.reload()
 
     } catch (err) {
-        console.log('err',err)
+        console.log('err', err)
     }
 }
 
-export const RestoreAll = (array,_id)=>async dispatch=>{
+export const RestoreAll = (array, _id) => async dispatch => {
     try {
-        let response = await axios.patch(`${url}/restore/${_id}`,{
-            arr : array
+        let response = await axios.patch(`${url}/restore/${_id}`, {
+            arr: array
         })
-        if(!response.data.msg){
-            dispatch({type:ActionTypes,payload:response.data})
+        if (!response.data.msg) {
+            dispatch({ type: ActionTypes, payload: response.data })
             window.location.reload()
         }
     } catch (err) {
-        console.log("err",err)
+        console.log("err", err)
     }
 }
 
-export const DraftMsg = (message,_id)=>async dispatch=>{
+export const DraftMsg = (message, _id) => async dispatch => {
     try {
-        let response = await axios.patch(`${url}/draft/${_id}`,{
-            msg:message
+        let response = await axios.patch(`${url}/draft/${_id}`, {
+            msg: message
         })
-        dispatch({type:ActionTypes.FETCH_MAILS,payload:response.data})
+        dispatch({ type: ActionTypes.FETCH_MAILS, payload: response.data })
         window.location.reload()
     } catch (err) {
-        console.log("err:",err)
+        console.log("err:", err)
     }
 }
